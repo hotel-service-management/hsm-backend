adf5793a7b78403344e4dba2c00f93cd10023d1b
Wiput Pootong <wiput.pootong@gmail.com>
2019-04-13 18:49:19 +0700

    :sparkles: [admin] add user fieldsets

--- users/admin.py
+++ users/admin.py
@@ -11,6 +11,52 @@ class CustomUserAdmin(UserAdmin):
     form = UserChangeForm
     model = User
     list_display = ['email', 'username', ]
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': ('email', 'password',)
+            }
+        ),
+        (
+            'Personal Info', {
+                'fields': ('first_name', 'last_name', 'gender', 'address',)
+            }
+        ),
+        (
+            'Permissions', {
+                'fields': ('is_staff', 'is_active')
+            }
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'username', 'email', 'password1', 'password2',
+                )
+            },
+
+        ),
+        (
+            'Personal Info',
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'first_name', 'last_name', 'gender', 'address',
+                )
+            }
+        ),
+        (
+            'Permission',
+            {
+                'classes': ('wide',),
+                'fields': ('is_staff', 'is_active')
+            }
+        )
+    )
 
 
 admin.site.register(User, CustomUserAdmin)
