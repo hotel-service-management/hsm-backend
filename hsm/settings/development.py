bbbbb6a44ca7133d647989ec99d586e037a20cb9
Wiput Pootong <wiput.pootong@gmail.com>
2019-04-19 23:43:05 +0700

    :bug: fix static

--- hsm/settings/development.py
+++ hsm/settings/development.py
@@ -145,9 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATICFILES_DIRS = (
-    ('', path.join(BASE_DIR, 'static')),
-)
+STATIC_ROOT = path.join(BASE_DIR, 'static')
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
