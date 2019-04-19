cfa0e7ad3169328c8bb681827ee412157403603b
Wiput Pootong <wiput.pootong@gmail.com>
2019-04-19 23:40:43 +0700

    :bug: fix static

--- hsm/settings/development.py
+++ hsm/settings/development.py
@@ -145,8 +145,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = path.join(BASE_DIR, 'static')
-
 STATICFILES_DIRS = (
     ('', path.join(BASE_DIR, 'static')),
 )
--- manage.py
+++ manage.py
@@ -3,7 +3,13 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hsm.settings.development')
+    PRODUCTION = os.environ.get('ENV') == 'production'
+
+    if PRODUCTION:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hsm.settings.production')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hsm.settings.development')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
