79141745b00fd03fdc895db6f6ad38858245ccf4
Wiput Pootong <wiput.pootong@gmail.com>
2019-05-07 02:12:58 +0700

    :ambulance: payment

--- payment/serializers.py
+++ payment/serializers.py
@@ -10,6 +10,8 @@ class PaymentSerializer(serializers.ModelSerializer):
 
     booking_id = serializers.PrimaryKeyRelatedField(queryset=Booking.objects.all(), write_only=True, source='booking')
 
+    payment_type = serializers.ChoiceField(write_only=True, choices=Payment.payment_type_choice)
+
     class Meta:
         model = Payment
         fields = '__all__'
