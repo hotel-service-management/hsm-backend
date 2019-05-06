a310c5b3de7f5443a993db6d48033e8dbae0ddb9
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2019-05-06 18:44:44 +0700

    :sparkles: Added filters on service and one-time order's total price

--- order/admin.py
+++ order/admin.py
@@ -7,17 +7,21 @@ from order.models import Order, Service
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'booking_detail', 'total_price']
+    readonly_fields = ['total_price']
 
     formfield_overrides = {
         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
     }
 
-
-admin.site.register(Order, OrderAdmin)
-
+    def save_model(self, request, obj, form, change):
+        """ NOTES: MUST DOUBLE-SAVE TO REALLY UPDATE TOTAL_PRICE """
+        super().save_model(request, obj, form, change)
+        Order.objects.filter(pk=obj.id).update(total_price=sum([i.price for i in Order.objects.get(pk=obj.id).service.all()]))
 
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ['id', 'type', 'title', 'price']
+    list_filter = ['type']
+    search_fields = ['title']
 
-
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Service, ServiceAdmin)
