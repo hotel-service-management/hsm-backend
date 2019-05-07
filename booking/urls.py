e337d9af2ed81917edb4b5588c0853c83942a711
Wiput Pootong <wiput.pootong@gmail.com>
2019-05-08 02:46:49 +0700

    :sparkles: add reset password

--- booking/urls.py
+++ booking/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls import url
 from django.urls import path
 
 from booking.views import BookingView, BookingsView, RoomsView, RoomView, BookingDetailView, PrivilegeView
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, include
 
 from .views import RegistrationAPIView, UserRetrieveUpdateAPIView, LoginAPIView
 from rest_framework_simplejwt import views as jwt_views
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^register/?$', RegistrationAPIView.as_view(), name='register'),
     path('login/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('login', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    url(r'^reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
